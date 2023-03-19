45382dd0cf682ff65604aec0032baa1494c9243c
Bair <73276298+zoxione@users.noreply.github.com>
2023-03-19 13:46:10 +0300

    init

---
+++ src/pages/api/sendMessages.ts
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Configuration, OpenAIApi } from "openai";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const configuration = new Configuration({
+    // apiKey: "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",
+    apiKey: req.body.settings.apiKey,
+  });
+  const openai = new OpenAIApi(configuration);
+
+  const completion = await openai.createChatCompletion({
+    model: req.body.settings.model,
+    messages: req.body.messages,
+    temperature: req.body.settings.temperature,
+    frequency_penalty: req.body.settings.frequency_penalty,
+    presence_penalty: req.body.settings.presence_penalty,
+    max_tokens: req.body.settings.max_tokens,
+  });
+  res.status(200).json({ result: completion.data });
+}
