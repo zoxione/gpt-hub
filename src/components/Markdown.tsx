9af74a2b950a0cfd3c3851f7d014c630aed5643f
Bair <73276298+zoxione@users.noreply.github.com>
2023-03-21 22:51:13 +0300

    fix

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -9,17 +9,14 @@ const Markdown = (props: IProps) => {
   return (
     <ReactMarkdown
       className="chat-bubble"
-      children={props.content}
       components={{
         code({ node, inline, className, children, ...props }) {
           const match = /language-(\w+)/.exec(className || "");
 
           return !inline && match ? (
-            <SyntaxHighlighter
-              language={match[1]}
-              PreTag="div"
-              children={String(children).replace(/\n$/, "")}
-            />
+            <SyntaxHighlighter language={match[1]} PreTag="div">
+              {String(children).replace(/\n$/, "")}
+            </SyntaxHighlighter>
           ) : (
             <code className={className ? className : ""} {...props}>
               {children}
@@ -27,7 +24,9 @@ const Markdown = (props: IProps) => {
           );
         },
       }}
-    />
+    >
+      {props.content}
+    </ReactMarkdown>
   );
 };
 
