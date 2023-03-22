be9642fa7947dcc22193fbdc08da8afa242cca80
Bair <73276298+zoxione@users.noreply.github.com>
2023-03-22 20:53:43 +0300

    one theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,4 +12,9 @@ module.exports = {
     extend: {},
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
+
+  // daisyUI config (optional)
+  daisyui: {
+    themes: ["dark"],
+  },
 };
